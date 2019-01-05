516adc2e9103c88e88def6dada534c6f17bec744
YousefKJM <UaSo0oF@macbook-pro.local>
2019-01-06 00:38:14 +0300

    fixing all the constraints and adding the nav bar

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -42,6 +42,8 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+//        subscribeToKeyboardNotifications()
+ 
 
     }
 
@@ -101,6 +103,30 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         return textField.resignFirstResponder()
     }
+    
+    
+    
+//    func subscribeToKeyboardNotifications() {
+//
+//    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+//    }
+//
+//    func unsubscribeFromKeyboardNotifications() {
+//
+//        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+//    }
+//
+//    @objc func keyboardWillShow(_ notification:Notification) {
+//
+//        view.frame.origin.y -= getKeyboardHeight(notification)
+//    }
+//
+//    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+//
+//        let userInfo = notification.userInfo
+//        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+//        return keyboardSize.cgRectValue.height
+//    }
  
 }
 
