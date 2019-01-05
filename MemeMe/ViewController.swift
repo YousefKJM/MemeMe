dabf27bdd7c70382510eaa79aec32773e2ffc5e0
YousefKJM <UaSo0oF@macbook-pro.local>
2019-01-05 21:58:15 +0300

    finish Picking an Image part

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+    }
+    
 }
 
