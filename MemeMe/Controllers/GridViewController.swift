847d5e64cab12d3259ac33c7dad08394295dafbf
YousefKJM <UaSo0oF@macbook-pro.local>
2019-01-09 17:10:28 +0300

    introduce new packaging way

--- MemeMe/Controllers/GridViewController.swift
+++ MemeMe/Controllers/GridViewController.swift
@@ -63,8 +63,42 @@ class GridViewController: UICollectionViewController, UICollectionViewDelegateFl
         cell.imageView.image = MemeStorage.get(indexPath.item).memedImage
         return cell
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Process selection
+        let detailViewController: MemeDetailsViewController = storyboard?.instantiateViewController(withIdentifier: "MemeDetailsViewController") as! MemeDetailsViewController
+        detailViewController.position = indexPath.item
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
 
     // MARK: UICollectionViewDelegate
+
+    // MARK: UICollectionViewDelegateFlowLayout Methods
+    
+    let itemsPerRow: CGFloat = 3
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
 
     /*
     // Uncomment this method to specify if the specified item should be highlighted during tracking
